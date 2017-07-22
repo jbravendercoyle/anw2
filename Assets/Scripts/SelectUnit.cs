49a09fb557e4cd939798ea30bcc838c6b3e26cb9
James Bravender-Coyle <jbravendercoyle@gmail.com>
2017-07-22 17:56:02 +1000

    Select Unit
    added a "select this unit" script

---
+++ Assets/Scripts/SelectUnit.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectUnit : MonoBehaviour {
+
+	GameObject thisUnit;
+	public bool SelectThisUnit;
+
+	// Use this for initialization
+	void Start () {
+		thisUnit = this.gameObject;
+		SelectThisUnit = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (SelectThisUnit == true) {
+			ClickToMove component = thisUnit.GetComponent<ClickToMove> ();
+			component.enabled = true;
+			thisUnit.transform.GetChild (0).gameObject.SetActive (true);
+		}
+
+		if (SelectThisUnit == false) {
+			ClickToMove component = thisUnit.GetComponent<ClickToMove> ();
+			component.enabled = false;
+			thisUnit.transform.GetChild (0).gameObject.SetActive (false); 
+	}
+}
+}
